0c365feb1f89d9fa4778ea39ab618bbd472daa93
williamdrums <williandrums@gmail.com>
2021-04-22 22:13:55 -0400

    interagindo com listas

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -2,6 +2,14 @@ namespace Course.ContentContext
 {
     public class CareerItem
     {
+        public CareerItem(int ordem, string title, string description, CourseOnline courseOnline)
+        {
+            Ordem = ordem;
+            Title = title;
+            Description = description;
+            CourseOnline = courseOnline;
+        }
+
         public int Ordem { get; set; }
 
         public string Title { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Course.ContentContext;
 
 namespace Course
@@ -16,19 +17,52 @@ namespace Course
             // career.Items.Add(new CareerItem());
             //  Console.WriteLine(career.TotalCourses);
 
-            var articles = new List<Article>();
-            articles.Add(new Article("Artigo sobre OOP", "orientacao-objeto"));
-            articles.Add(new Article("Artigo sobre C#", "csharp"));
-            articles.Add(new Article("Artigo sobre .NET", "dotnet"));
+            // var articles = new List<Article>();
+            // articles.Add(new Article("Artigo sobre OOP", "orientacao-objeto"));
+            // articles.Add(new Article("Artigo sobre C#", "csharp"));
+            // articles.Add(new Article("Artigo sobre .NET", "dotnet"));
 
-            foreach (var article in articles)
+
+            var courses = new List<CourseOnline>();
+            var courseOOP = new CourseOnline("Fundamentos OOP", "fundamentos-oop");
+            var courseCsharp = new CourseOnline("Fundamentos C#", "fundamentos-csharp");
+            var courseAspNet = new CourseOnline("Fundamentos ASP.NET", "fundamentos-aspnet");
+
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotNet = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
+            var careerItem1 = new CareerItem(1, "Comece aqui", "", null);
+            var careerItem3 = new CareerItem(3, "Aprenda .NET", "", null);
+
+            careerDotNet.Items.Add(careerItem2);
+             careerDotNet.Items.Add(careerItem3);
+              careerDotNet.Items.Add(careerItem1);
+            careers.Add(careerDotNet);
+
+
+
+
+            foreach (var career in careers)
             {
-                Console.WriteLine(article.Id);
-                Console.WriteLine(article.Title);
-                Console.WriteLine(article.Url);
+                Console.WriteLine(career.Title);
+
+                foreach (var item in career.Items.OrderBy(x => x.Ordem))
+                {
+                    Console.WriteLine($"{item.Ordem} - {item.Title}");
+                }
             }
 
 
+            // foreach (var article in articles)
+            // {
+            //     Console.WriteLine(article.Id);
+            //     Console.WriteLine(article.Title);
+            //     Console.WriteLine(article.Url);
+            // }
         }
     }
 }
